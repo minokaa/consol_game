e3da4247f434f54ce616243ab5c1e0f3b0cb97ca
Minokaa <100104417+minokaa@users.noreply.github.com>
2024-02-25 18:18:19 +0300

    Update main.cpp
    added:
    healthup
    money gains
    rise in difficulty
    fix:
    recoded system menu

--- main.cpp
+++ main.cpp
@@ -5,18 +5,21 @@ int main() {
 	int HP = 10;
 	int damage = 0;
 	int HP_grr = 20;
-	char input_menu;
+	int input_menu;
 	short status_menu = 1;
 	int c_damage;
 	int rand_int;
-	char shop;
+	int shop;
 	int d1 = 0;
 	int p1 = 0;
 	int money = 10;
+	int HP_grr_default = 20;
+	int money_def = 5;
+	int damage_def = 3;
 
 	std::cout << "HP: " << HP << std::endl
 		<< "HP_grr: " << HP_grr << std::endl;;
-	
+
 	while (status_menu != 0) {
 		srand(time(NULL));
 
@@ -25,14 +28,17 @@ int main() {
 		std::cin >> input_menu;
 		switch (input_menu)
 		{
-		case 'h':
+		case 1:
 			status_menu = 1;
 			break;
-		case 'q':
+		case 3:
 			status_menu = 0;
 			break;
-		case 's':
+		case 2:
 			status_menu = 2;
+			break;
+		default:
+			status_menu = 0;
 			break;
 		}
 		if (status_menu == 1) {
@@ -42,7 +48,7 @@ int main() {
 				damage = 0;
 			}
 			else if (rand_int >= 4) {
-				damage = 1 + rand() % 5;
+				damage = 1 + rand() % damage_def;
 				damage -= p1;
 				if (damage < 0) {
 					damage = 0;
@@ -57,13 +63,19 @@ int main() {
 				<< "HP_grr: " << HP_grr << std::endl;
 			if (HP <= 0) {
 				std::cout << std::endl;
-				std::cout << "GAME OVER";
+				std::cout << "BALLS OVER";
 				status_menu = 0;
 			}
 			if (HP_grr <= 0) {
 				std::cout << std::endl;
-				std::cout << "WIN";
-				status_menu = 0;
+				std::cout << "WIN" << std::endl;
+				HP_grr_default *= 1.3;
+				damage_def *= 1.3;
+				HP_grr = HP_grr_default;
+				std::cout << "HP: " << HP << std::endl
+					<< "HP_grr: " << HP_grr << std::endl;;
+				money += (money_def * 1.7);
+				money_def *= 1.5;
 			}
 		}
 		else if (status_menu == 2) {
@@ -72,16 +84,20 @@ int main() {
 				<< "SHOP" << std::endl
 				<< "================" << std::endl
 				<< "money: " << money << std::endl
-				<< "damage/protection" << std::endl;
+				<< "damage/protection/healthup" << std::endl;
 			std::cin >> shop;
 			switch (shop) {
-			case 'd':
+			case 1:
 				if (money >= 10) {
 					d1++;
 					money -= 10;
 				}
+				else {
+					std::cout << std::endl;
+					std::cout << "NO MONEY" << std::endl;
+				}
 				break;
-			case 'p':
+			case 2:
 				if (money >= 13) {
 					p1++;
 					money -= 13;
@@ -91,8 +107,20 @@ int main() {
 					std::cout << "NO MONEY" << std::endl;
 				}
 				break;
+			case 3:
+				if (money >= 5) {
+					HP += 3;
+					money -= 5;
+				}
+				else {
+					std::cout << std::endl;
+					std::cout << "NO MONEY" << std::endl;
+				}
+				break;
+			default:
+				status_menu = 0;
+				break;
 			}
-			
 		}
 	}
 
